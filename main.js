3ed74b6003692076825d50ea412c75e5a0219a23
chelsey <chelseyfrank@MacBook-Pro.local>
2015-01-13 00:57:29 -0800

    Re-intro of photos (& animation) in My Work, smaller My Play photos

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 $(function () {
   $('.fa-navigation').delay(1700).fadeTo(1000, 1, function() {
-    $('.fa-navigation').addClass('pulse')
+    $('.fa-navigation').addClass('pulse');
   });
 
   $(window).scroll(function(){
-    $('.fa-navigation').removeClass('pulse')
+    $('.fa-navigation').each(function(){
+      var navigatorPosition = $(this).offset().top;
+      var topOfWindow = $(window).scrollTop();
 
-    if ($(window).scrollTop() > $('.fade-container').offset().top + 50) {
-      $('.fa-navigation').animate({opacity:0});
-    }
+      if (topOfWindow > navigatorPosition-100) {
+        $(this).removeClass('pulse');
+      }
+    });
+
+    $('.portfolio-row').each(function(){
+      var navigatorPosition = $(this).offset().top;
+      var topOfWindow = $(window).scrollTop();
+
+      if (topOfWindow > navigatorPosition - $(this).height() ) {
+        $(this).addClass('reveal');
+        $(this).css({opacity: 1});
+      }
+    });
   });
 });
